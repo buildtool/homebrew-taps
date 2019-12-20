c1f0064c30b79a81b275a5a3b2276c24c634e988
peter-stc <peter@sparetimecoders.com>
2019-12-20 09:25:56 +0100

    Brew formula update for build-tools version v0.0.9

--- Formula/build-tools.rb
+++ Formula/build-tools.rb
@@ -2,16 +2,16 @@
 class BuildTools < Formula
   desc ""
   homepage ""
-  version "0.0.8"
+  version "0.0.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/buildtool/build-tools/releases/download/v0.0.8/build-tools_0.0.8_Darwin_x86_64.tar.gz"
-    sha256 "da441cfacafbad0699b01cf93859690a1a1fe0109bc8d4b211276ab94ac70872"
+    url "https://github.com/buildtool/build-tools/releases/download/v0.0.9/build-tools_0.0.9_Darwin_x86_64.tar.gz"
+    sha256 "30b22b8a781784f490806bd368b10b508b4db210ad0595d59ca5ab1f8a928951"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/buildtool/build-tools/releases/download/v0.0.8/build-tools_0.0.8_Linux_x86_64.tar.gz"
-      sha256 "db0a703c938b596ac76a153c259435eb66bf823dadbea8a8108ef00111bc6ec0"
+      url "https://github.com/buildtool/build-tools/releases/download/v0.0.9/build-tools_0.0.9_Linux_x86_64.tar.gz"
+      sha256 "a5e3e7cefb2cd42083b8e830126db0eb277601ee4b42d9c4f24863320db61c7c"
     end
   end
 
