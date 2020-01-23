31f7f5086028420b7c3beda3c4e074dc0c4196c4
peter-stc <peter@sparetimecoders.com>
2020-01-23 17:03:53 +0100

    Brew formula update for build-tools version v0.0.12

--- Formula/build-tools.rb
+++ Formula/build-tools.rb
@@ -2,16 +2,16 @@
 class BuildTools < Formula
   desc ""
   homepage ""
-  version "0.0.11"
+  version "0.0.12"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/buildtool/build-tools/releases/download/v0.0.11/build-tools_0.0.11_Darwin_x86_64.tar.gz"
-    sha256 "147c0674411d9677e753b04f69d96cb67bfa4eaedcb33c0fd2cd496bcd245e07"
+    url "https://github.com/buildtool/build-tools/releases/download/v0.0.12/build-tools_0.0.12_Darwin_x86_64.tar.gz"
+    sha256 "8a16d6138311eb6e56b09e210bf23551aa190d1d728ec135d859aa7fc1d7ecac"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/buildtool/build-tools/releases/download/v0.0.11/build-tools_0.0.11_Linux_x86_64.tar.gz"
-      sha256 "ba8c2268ba25d677a623be8238409187584d569f25da3058dee222d6de988cff"
+      url "https://github.com/buildtool/build-tools/releases/download/v0.0.12/build-tools_0.0.12_Linux_x86_64.tar.gz"
+      sha256 "00ee1d6d6b0bf6e665ece2cf016d3b4f5ba10c1911e9a9e0a7d48217d20b58a9"
     end
   end
 
