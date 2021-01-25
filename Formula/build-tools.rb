263321df1f0dbc686227793e0d88baca5197a881
peter-stc <peter@sparetimecoders.com>
2021-01-25 15:44:21 +0100

    Brew formula update for build-tools version v0.0.26

--- Formula/build-tools.rb
+++ Formula/build-tools.rb
@@ -5,16 +5,16 @@
 class BuildTools < Formula
   desc ""
   homepage "https://buildtools.io/"
-  version "0.0.25"
+  version "0.0.26"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/buildtool/build-tools/releases/download/v0.0.25/build-tools_0.0.25_Darwin_x86_64.tar.gz"
-    sha256 "d0e1fb3a64d6ff226d053b2111a535ef59df91c72fb039103796b5c6c4d2afc1"
+    url "https://github.com/buildtool/build-tools/releases/download/v0.0.26/build-tools_0.0.26_Darwin_x86_64.tar.gz"
+    sha256 "3b55425c318746cc56744af15114582d5a36d62dcecb350ee8d3c1bd9de9256c"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/buildtool/build-tools/releases/download/v0.0.25/build-tools_0.0.25_Linux_x86_64.tar.gz"
-    sha256 "a25484c1a26c5c819dc203e62ebf0fdcacac8966890ca4986b16403cbc1c70c1"
+    url "https://github.com/buildtool/build-tools/releases/download/v0.0.26/build-tools_0.0.26_Linux_x86_64.tar.gz"
+    sha256 "eff4a318632348462ccb58fa82aff22013853d57ba23ac8ec8fa8370e225a174"
   end
 
   def install
